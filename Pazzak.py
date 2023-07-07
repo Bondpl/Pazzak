3c76ee26b150533ddb42441e3d3d5f41f78ccb78
Bondpl <rafal.majewski92@gmail.com>
2023-07-07 20:58:29 +0200

    no ulepszona ugulem cale te

--- Pazzak.py
+++ Pazzak.py
@@ -20,19 +20,43 @@ DECK = [
 DECK_player = []
 DECK_enemy = []
 
-EXTRA = [1, 2, 3, 5, 6]
+EXTRA_player = [-1, 2, 3, 5, 6]
+EXTRA_enemy = [
+    -2,
+    1,
+    3,
+    5,
+    6,
+]
 END = 20
 pas = False
 
-
-score_player = int(0)
-score_enemy = int(0)
+score_player = 0
+score_enemy = 0
 
 
 def choose_card_player():
     card_player = random.choice(DECK)
     DECK_player.append(card_player)
     DECK.remove(card_player)
+
+def choose_extra_card():
+        if EXTRA_player:
+            print("twoje karty",EXTRA_player)
+            choice = input("do you want do pick extra card? (y/n): ")
+            if choice.lower() == "y":
+                print("choose the card\t", EXTRA_player)
+                print("index of cards\t  1  2  3  4  5")
+                choice = int(input("\nwhat card do you choose "))
+
+                while choice > len(EXTRA_player) or choice <= 0:
+                    choice = int(input("wrong index, choose correct one"))
+
+                EXTRA_card = EXTRA_player[choice - 1]
+                DECK_player.append(EXTRA_card)
+                EXTRA_player.remove(EXTRA_card)
+            else:
+                retrun: 0
 
 
 def choose_card_enemy():
@@ -41,7 +65,7 @@ def choose_card_enemy():
     DECK.remove(card_enemy)
 
 
-while not pas and sum(DECK_player) < END:
+while not pas and sum(DECK_player) < END and sum(DECK_enemy) < END:
     print(
         "wybrane karty :",
         DECK_player,
@@ -59,33 +83,52 @@ while not pas and sum(DECK_player) < END:
         score_enemy,
     )
 
-    choise = input("1 - hit\t2 - pass\n")
+    choice = input("1 - hit\t2 - pass\n")
 
-    if choise == "1":
+    if choice == "1":
         choose_card_player()
+        print(
+        "wybrane karty :",
+        DECK_player,
+        "\t  wartosc kart:",
+        sum(DECK_player),
+        "\tscore :",
+        score_player,
+    )
+        choose_extra_card()
         if sum(DECK_enemy) < END:
             choose_card_enemy()
 
-    elif choise == "2":
+
+    elif choice == "2":
         while sum(DECK_enemy) < END and sum(DECK_enemy) < sum(DECK_player):
-            choose_card_enemy
+            choose_card_enemy()
             if sum(DECK_enemy) == END:
                 score_enemy = score_enemy + 1
                 break
             elif sum(DECK_enemy) > END:
                 score_enemy = score_enemy - 1
         pas = True
+    else:
+        choice = input("1 - hit\t2 - pass\n")
+
+    if sum(DECK_player) == END:
+        score_player += 1
+    elif sum(DECK_enemy) == END:
+        score_enemy += 1
 
     if sum(DECK_player) > END:
         print("PRZEGRALES!!!")
         score_player = score_player - 1
-    elif sum(DECK_player) == END:
+    elif sum(DECK_player) == END and sum(DECK_enemy) > END:
         print("WYGRALES!!!")
         score_player = score_player + 1
+    elif sum(DECK_player) == END and sum(DECK_enemy) == END:
+        print("REMIS!!!")
 
-    if sum(DECK_enemy) > END:
-        print("PRZEGRALES!!!")
-        score_enemy = score_enemy - 1
-    elif sum(DECK_enemy) == END:
-        print("WYGRALES!!!")
-        score_enemy = score_enemy + 1
+    # if sum(DECK_enemy) > END:
+    #     print("PRZEGRALES!!!")
+    #     score_enemy = score_enemy - 1
+    # elif sum(DECK_enemy) == END:
+    #     print("WYGRALES!!!")
+    #     score_enemy = score_enemy + 1
